85365c9a57e3217f7ddd3efa37e9b25ad5dcc268
unknown <teslimjimoh191@gmail.com>
2022-11-24 13:55:05 +0000

    added a new file

---
+++ starting-a-server/index.js
@@ -0,0 +1,18 @@
+/*
+ * Primary file for API
+ *
+ */
+
+// Dependencies
+var http = require('http');
+
+ // Configure the server to respond to all requests with a string
+var server = http.createServer(function(req,res){
+  console.log(req);
+  res.end('Hello World!\n');
+});
+
+// Start the server
+server.listen(3000,function(){
+  console.log('The server is up and running now');
+});
