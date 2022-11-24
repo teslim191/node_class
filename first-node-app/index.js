d6f830d639e6f2c57ffa29f199509a56b0eeccf3
unknown <teslimjimoh191@gmail.com>
2022-11-24 14:07:45 +0000

    added a new file

--- first-node-app/index.js
+++ first-node-app/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+// let current_year = 2022
+// let year_born = 1997
+// console.log(`my name is teslim and i am ${current_year - year_born} years`)
+
+function myAge(current_year, year_born){
+    return `my name is teslim and i am ${current_year - year_born} years`
+}
+
+console.log(myAge(2022,1990))
